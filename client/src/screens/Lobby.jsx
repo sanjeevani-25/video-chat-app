6ae4eb5cc0b989378ba9e97a65f642accd797bd9
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2023-11-27 10:33:34 +0530

    socket context

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 export const LobbyScreen = () => {
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleSubmitForm = useCallback(
+    (e) => {
+      e.preventdefault();
+      console.log({ email, room });
+    },
+    [email, room]
+  );
+
   return (
     <div>
-      <h1>Lobby Screen</h1>
+      <h1>Lobby</h1>
+      <form onSubmit={handleSubmitForm}>
+        <label htmlFor="email">Email ID</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <label htmlFor="room">Room Number</label>
+        <input
+          type="text"
+          id="room"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <br />
+        <button>Join</button>
+      </form>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 const { Server } = require("socket.io");
 
-const io = new Server(8000);
+const io = new Server(8000, { cors: true });
 
 io.on("connection", (socket) => {
   console.log("Socket Connected", socket.id);
 });
-
-
